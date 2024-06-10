c53524b9bc5637395b984423abcc384b33919c05
Edward Zhou <esz7923@nyu.edu>
2024-06-09 17:13:48 -0700

    added react routing boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { browserRouter, Routes, Route, BrowserRouter } from 'react-router-dom'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App/>} />
+      </Routes>
+    </BrowserRouter>
+  
   </React.StrictMode>
 );
