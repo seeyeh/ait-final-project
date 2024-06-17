3e4b343ed77de4cee272873773be3292f6a61c3f
Edward Zhou <esz7923@nyu.edu>
2024-06-17 15:23:14 -0700

    add exercise controller delete operation, add enum validation for post operation parameters

--- server/controllers/exercisesController.js
+++ server/controllers/exercisesController.js
@@ -3,13 +3,30 @@ import Attempt from '../models/Attempt.js';
 import asyncHandler from 'express-async-handler';
 import Exercise from '../models/Exercise.js';
 
+const ExerciseSchemaFields = Object.freeze({
+    _id:            Symbol("_id"),
+    parentUser:     Symbol('parentUser'),
+    name:           Symbol('name'),
+    description:    Symbol('description'),
+    notes:          Symbol('notes'),
+    video:          Symbol('video'),
+    photos:         Symbol('photos'),
+    substitutions:  Symbol('substitutions'),
+    history:        Symbol('history'),
+    stats:          Symbol('stats'),
+})
 // @desc Get a specific exercise by quering through name or get ALL exercises
 // @route GET /exercises
 // @access Private
 const getExercise = asyncHandler(async (req,res) => {
     const { parentUser } = req.query; // extract key/value pair of 'name' key from query string
-    
     if(!parentUser) return res.status(400).json({ message: 'parentUser field required'});
+
+    // check if all fields of query are valid
+    for(let field of req.query) {
+        if(!field in ExerciseSchemaFields) res.status(400).json({ message: 'Invalid query field'});
+    }
+
     const exercise = await Exercise.find(req.query).lean();
     if(!exercise?.length){
         return res.status(400).json({ message: `No exercise found`});
@@ -36,7 +53,7 @@ const createNewExercise = asyncHandler(async (req,res) => {
     if(exerciseParams.substitutions) {
         exerciseParams.substitutions = await Promise.all(exerciseParams.substitutions.map(
             async (subName) => {
-                return Exercise.findOne({ parentUser, 'name': subName }).select("_id");
+                return Exercise.findOne({ parentUser, 'name': subName }).select('_id');
             }
         ))         
         console.log(exerciseParams.substitutions);
@@ -59,12 +76,53 @@ const createNewExercise = asyncHandler(async (req,res) => {
 // @route PATCH /exercises
 // @access Private
 const updateExercise = asyncHandler(async (req,res) => {
+
+    /* REFACTORED PATCH APPROACH
+    
+    const {ops, path, data} = req.body;
+    
+    // Confirm data (at a minimum, id and username have to be in req body)
+    if(!path.id || !path.username){
+        return res.status(400).json({ message: 'ID and username fields are required' });
+    }
+    switch(ops) {
+        case 'add':
+            switch(path.location)
+                case 'split': user.splits.push(data); break;
+                case 'template': user.templates.push(data); break;
+
+            OR 
+
+            if(data.split) user.splits.push(data.split)
+            if(data.template) user.templates.push(data.templates)
+            if(data.exercise) user.exercises.push(data.exercises)
+            
+        case 'replace':
+            if(data.username) user.username = data.username
+            ...
+
+    }
+     
+    */
 })
 
 // @desc Delete an exercise
 // @route DELETE /exercises
 // @access Private
 const deleteExercise = asyncHandler(async (req,res) => {
+    const { parentUser, name } = exerciseParams;
+    // Confirm data (parentUser and name required)
+    if(!parentUser || !name) {
+        return res.status(400).json({ message: 'parentUser and exercise name fields are required'});
+    }
+
+    const exercise = await Exercise.find(req.query);
+    if(!exercise?.length){
+        return res.status(400).json({ message: `No exercise found`});
+    }
+
+    const result = await exercise.deleteOne()
+    res.json(`Exercise ${result.name} deleted`);
 })
 
 export default {
