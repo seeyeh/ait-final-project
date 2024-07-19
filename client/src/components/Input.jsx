f31aa70fb1aac5f8ea6738551c93feed333a032c
Edward Zhou <esz7923@nyu.edu>
2024-07-19 15:44:13 -0700

    modify input component as forwardref

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,27 +1,34 @@
+import { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-
-function Input(props) {
+import cn from '../lib/utils';
+const Input = forwardRef(({ id, className, header, ...props }, ref) => {
   // props needed: header, type, placeholder, handleChange, name, value
   return (
     <div>
-      <h3 className="input--header">{props.header}</h3>
+      <label htmlFor={id} className="input--header">
+        {header}
+      </label>
       <input
-        type={props.type}
-        placeholder={props.placeholder}
-        onChange={props.handleChange} // the parent Form component that holds this Input should be the one holding a formData state and also a function that changes that state
-        name={props.name}
-        value={props.value} // props.value = formData.[whatever the name of the input being updated in formData state in the parent Form component] (e.g. formData.firstName)
+        id={id}
+        className={cn('border-pink-10 flex bg-grayscale-90', className)}
+        {...props}
+        ref={ref}
+        // type={props.type}
+        // placeholder={props.placeholder}
+        // onChange={props.handleChange} // the parent Form component that holds this Input should be the one holding a formData state and also a function that changes that state
+        // name={props.name}
+        // value={props.value} // props.value = formData.[whatever the name of the input being updated in formData state in the parent Form component] (e.g. formData.firstName)
       />
     </div>
   );
-}
+});
+Input.displayName = 'Input';
 Input.propTypes = {
+  id: PropTypes.string,
   header: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
-  handleChange: PropTypes.func,
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired
+  className: PropTypes.string
 };
 
 export default Input;
+
+// <Input className="bg-white"/>
