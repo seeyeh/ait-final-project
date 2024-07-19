1b422e010137fdc26ee3801f23d67b714aa47206
William Xie <wx2114@nyu.edu>
2024-07-18 17:14:48 -0700

    Added basic JSX ccode, with bare minimum props and set up propTypes. Still need to fix input--header so that it's acctually a label for the input element. For Input.jsx

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,5 +1,27 @@
-function Input() {
-  return <div>Input</div>;
+import PropTypes from 'prop-types';
+
+function Input(props) {
+  // props needed: header, type, placeholder, handleChange, name, value
+  return (
+    <div>
+      <h3 className="input--header">{props.header}</h3>
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={props.handleChange} // the parent Form component that holds this Input should be the one holding a formData state and also a function that changes that state
+        name={props.name}
+        value={props.value} // props.value = formData.[whatever the name of the input being updated in formData state in the parent Form component] (e.g. formData.firstName)
+      />
+    </div>
+  );
 }
+Input.propTypes = {
+  header: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  handleChange: PropTypes.func,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired
+};
 
 export default Input;
