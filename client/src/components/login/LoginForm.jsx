970eb83f14515a75bc976a3617ed031e7ee96c8f
Edward Zhou <esz7923@nyu.edu>
2024-07-19 10:59:33 -0700

    fix file imports, add boilerplate

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -1,5 +1,14 @@
+import Button from '../Button';
+import Input from '../Input';
+
 function LoginForm() {
-  return <div>Placeholder Login Form</div>;
+  return (
+    <div>
+      Placeholder Login Form
+      <Button />
+      <Input header="Testing" type="text" name="Test" value="Test" />
+    </div>
+  );
 }
 
 export default LoginForm;
--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,5 +1,12 @@
-const Login = () => {
-  return <h1>Login</h1>;
-};
+import LoginForm from '../components/login/LoginForm';
+
+function Login() {
+  return (
+    <div>
+      Login
+      <LoginForm />
+    </div>
+  );
+}
 
 export default Login;
