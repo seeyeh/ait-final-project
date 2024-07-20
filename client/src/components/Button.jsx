208c7c49836866903af92872bc3eb9cee33e65c5
Edward Zhou <esz7923@nyu.edu>
2024-07-20 16:38:54 -0700

    add cva dependency, add variable button component

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,5 +1,43 @@
-function Button() {
-  return <div>Button</div>;
-}
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '../lib/utils';
+import { cva } from 'class-variance-authority';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center transition-colors rounded-full',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-grayscale-80 text-grayscale-25 hover:bg-grayscale-5 hover:text-grayscale-80',
+        pink: 'bg-pink-40 text-grayscale-90  hover:bg-grayscale-90 hover:text-pink-40 hover:outline hover:outline-pink-40'
+      },
+      size: {
+        default: 'px-3 py-0.5 text-base'
+      }
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default'
+    }
+  }
+);
+
+const Button = forwardRef(({ variant, size, className, ...props }, ref) => {
+  return (
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+
+Button.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.string,
+  size: PropTypes.string
+};
+Button.displayName = 'Button';
 
 export default Button;
--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -5,7 +5,10 @@ function LoginForm() {
   return (
     <div className="m-10 bg-grayscale-90 max-w-md p-4 text-grayscale-5">
       Placeholder Login Form
-      <Button />
+      <div>
+        <Button variant="pink">Next</Button>
+        <Button>Create an account</Button>
+      </div>
       <Input id="username" header="Username" type="text" name="Test" />
       <Input id="password" header="Password" type="text" name="Test" />
     </div>
