8dd54eba565e4d06a177ad478bebdc24d377df64
William Xie <wx2114@nyu.edu>
2024-07-20 18:41:22 -0700

    Added some padding to pink button, adjusted width and height of whole login div

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -3,11 +3,12 @@ import Input from '../Input';
 
 function LoginForm() {
   return (
-    <div className="bg-grayscale-90 w-[32rem] h-[36rem] rounded-4xl mb-8 shadow-glow-2xl shadow-grayscale-90 p-4 text-grayscale-5">
+    <div className="bg-grayscale-90 w-[20rem] h-[23rem] rounded-4xl mb-8 shadow-glow-2xl shadow-grayscale-90 p-5 text-grayscale-5">
+      <h1 className="text-5xl mb-6">Login</h1>
       <Input id="username" header="Username" type="text" name="Test" />
-      <Input id="password" header="Password" type="text" name="Test" />
+      <Input id="password" header="Password" className="mb-[4rem]" type="text" name="Test" />
       <div>
-        <Button variant="pink" size="lg">
+        <Button variant="pink" size="lg" className="mr-1">
           Next
         </Button>
         <Button size="lg">Create an account</Button>
