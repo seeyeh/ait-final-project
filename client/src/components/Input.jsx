7c0d988fcbff56054c94cf42e3b8c1aadc44c690
Edward Zhou <esz7923@nyu.edu>
2024-07-22 17:35:14 -0700

    fix input component styling

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -4,27 +4,19 @@ import { cn } from '../lib/utils';
 const Input = forwardRef(({ id, className, header, ...props }, ref) => {
   // props needed: header, type, placeholder, handleChange, name, value
   return (
-    <div className="group mb-5 mr-auto">
+    <div className={cn('flex flex-col-reverse w-56', className)}>
+      <input
+        id={id}
+        className="peer -mt-1 bg-grayscale-90 text-grayscale-5 border-grayscale-80 p-2 rounded-b-2xl rounded-tr-2xl border-2 w-full focus:outline-none focus:border-grayscale-25 transition-colors"
+        {...props}
+        ref={ref}
+      />
       <label
         htmlFor={id}
-        className="input--header bg-grayscale-80 p-1 pt-1 pb-[0.35rem] px-2 text-grayscale-5 focus:bg-grayscale-90 focus:text-grayscale-5"
+        className="w-fit bg-grayscale-80 p-2 pt-1 text-grayscale-5 focus:text-grayscale-5 peer-focus:bg-grayscale-25 peer-focus:text-grayscale-80 transition-colors"
       >
         {header}
       </label>
-      <input
-        id={id}
-        className={cn(
-          'bg-grayscale-90 text-grayscale-5 border-grayscale-80 px-2 pt-1 pb-2 mt-[3px] rounded-b-2xl rounded-tr-2xl border-2 w-[15rem] focus:outline-none flex focus:border-grayscale-25 focus:z-10 focus:transition-colors',
-          className
-        )}
-        {...props}
-        ref={ref}
-        // type={props.type}
-        // placeholder={props.placeholder}
-        // onChange={props.handleChange} // the parent Form component that holds this Input should be the one holding a formData state and also a function that changes that state
-        // name={props.name}
-        // value={props.value} // props.value = formData.[whatever the name of the input being updated in formData state in the parent Form component] (e.g. formData.firstName)
-      />
     </div>
   );
 });
--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -5,9 +5,9 @@ function LoginForm() {
   return (
     <div className="bg-grayscale-90 w-[32rem] h-[36rem] rounded-4xl mb-8 shadow-glow-2xl shadow-grayscale-90 p-8 text-grayscale-5 flex flex-col gap-4">
       <h1 className="text-7xl">Login</h1>
-      <div className="w-full flex-1 flex flex-col">
-        <Input id="username" header="Username" type="text" name="Test" />
-        <Input id="password" header="Password" type="text" name="Test" />
+      <div className="w-full flex-1 flex flex-col gap-4">
+        <Input id="username" header="Username" className="w-full" />
+        <Input id="password" header="Password" className="w-full" />
         <a href="/login" className="text-grayscale-70 self-end">
           Forgot password?
         </a>
