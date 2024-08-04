25e87c1ea0ab50e5e38552132db6edf3ac1b6b7b
Edward Zhou <esz7923@nyu.edu>
2024-08-03 18:21:26 -0700

    make prettier config consistent

--- client/src/routes/DashLayout.jsx
+++ client/src/routes/DashLayout.jsx
@@ -3,7 +3,7 @@ import DashSidebar from '../components/dashboard/DashSidebar';
 
 function DashLayout() {
   return (
-    <div className="flex max-w-screen max-h-screen min-h-screen h-screen w-screen absolute inset-0">
+    <div className="max-w-screen absolute inset-0 flex h-screen max-h-screen min-h-screen w-screen">
       <DashSidebar />
       <main className="px-12 pt-12">
         <Outlet />
