66338bde66b22d5cbd54e556d1cef84291a86176
Edward Zhou <esz7923@nyu.edu>
2024-08-22 13:26:28 -0700

    fixed undefined local storage blocking app

--- client/src/context/AuthProvider.jsx
+++ client/src/context/AuthProvider.jsx
@@ -5,7 +5,12 @@ const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
-  const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist")) || false);
+  const [persist, setPersist] = useState(() => {
+    const persistLogin = localStorage.getItem('persist');
+    return persistLogin && persistLogin !== 'undefined'
+      ? JSON.parse(persistLogin)
+      : false;
+  });
 
   return (
     <AuthContext.Provider value={{ auth, setAuth, persist, setPersist }}>
