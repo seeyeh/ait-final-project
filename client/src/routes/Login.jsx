7104e31657bebbd836f14824d96a5b68a8df20f3
Edward Zhou <esz7923@nyu.edu>
2024-09-03 23:40:47 -0400

    added redirect to dash on no auth routes

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,7 +1,18 @@
 import LoginForm from '@/components/auth/LoginForm';
+import useAuth from '@/hooks/useAuth';
+import { Navigate, useLocation } from 'react-router-dom';
 
 function Login() {
-  return (
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return auth?.accessToken ? (
+    <Navigate
+      to="/dash"
+      state={{ from: location }}
+      replace
+    />
+  ) : (
     <div className="flex min-h-screen w-full justify-center px-4 py-16">
       <LoginForm />
     </div>
--- client/src/routes/SignUp.jsx
+++ client/src/routes/SignUp.jsx
@@ -1,7 +1,18 @@
 import SignUpForm from '@/components/auth/SignUpForm';
+import useAuth from '@/hooks/useAuth';
+import { Navigate, useLocation } from 'react-router-dom';
 
 function SignUp() {
-  return (
+  const { auth } = useAuth();
+  const location = useLocation();
+
+  return auth?.accessToken ? (
+    <Navigate
+      to="/dash"
+      state={{ from: location }}
+      replace
+    />
+  ) : (
     <div className="flex min-h-screen w-full justify-center px-4 py-16">
       <SignUpForm />
     </div>
